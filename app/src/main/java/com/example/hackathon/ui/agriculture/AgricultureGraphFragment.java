a986b83e3391173908c34439824c3b24248610d7
Soham Bhattacharjee <soham.onlyn@gmail.com>
2022-04-23 17:35:10 -0700

    sub fragments

---
+++ app/src/main/java/com/example/hackathon/ui/agriculture/AgricultureGraphFragment.java
@@ -0,0 +1,18 @@
+package com.example.hackathon.ui.agriculture;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.hackathon.R;
+
+public class AgricultureGraphFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_agriculture_graph, container, false);
+    }
+}
